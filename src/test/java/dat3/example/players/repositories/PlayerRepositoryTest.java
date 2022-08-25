18b7a7343264b59a3cbc34da65c89e16f41da8e8
PillProductions <lukas.c.l.nielsen@gmail.com>
2022-08-25 15:18:11 +0200

    fejl

---
+++ src/test/java/dat3/example/players/repositories/PlayerRepositoryTest.java
@@ -0,0 +1,12 @@
+package dat3.example.players.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerRepositoryTest {
+    @Test
+    void fail(){
+        assertEquals(1, 0);
+    }
+}
