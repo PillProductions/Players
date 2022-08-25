e00411b6a5622a00d58884f9dd6fb91d9b34b7e3
PillProductions <lukas.c.l.nielsen@gmail.com>
2022-08-25 15:09:56 +0200

    Initial commit

---
+++ src/test/java/dat3/example/players/PlayersApplicationTests.java
@@ -0,0 +1,15 @@
+package dat3.example.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@DataJpaTest
+class PlayersApplicationTests {
+    @Test
+    void contextLoads() {
+        //this is a test
+    }
+
+}
